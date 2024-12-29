0e5a97ef68ccb0dbf517b341f33ca6c277aef3b4
Steven Espejo <55455142+C0C045@users.noreply.github.com>
2024-12-28 21:33:52 -0500

    Neotree hidden files false

--- LazyVim Config/lua/plugins/ui.lua
+++ LazyVim Config/lua/plugins/ui.lua
@@ -87,4 +87,20 @@ return {
       }
     end,
   },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    config = function ()
+      require("neo-tree").setup({
+        filesystem = {
+          filtered_items = {
+            visible = false, -- when true, they will just be displayed differently than normal items
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_hidden = false,
+          },
+        },
+      })
+    end
+  }
 }
